cc0989a06631822e6cf54c6ebcbd9e584a2e3c21
Richard Chou <richard@ezyraise.com>
2023-06-06 16:00:16 +1000

    simple ui

--- Gemfile
+++ Gemfile
@@ -51,6 +51,8 @@ gem "bootsnap", require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  gem 'pry-byebug', '~> 3.10.1'
+  gem 'pry-rails', '~> 0.3.9'
 end
 
 group :development do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all.order(created_at: :desc)
+  end
+
+  def show
+
+  end
+
+  def destroy
+    @user = User.find(params['id'])
+    @user.destroy
+    
+    respond_to do |format|
+      format.html { redirect_to users_path, notice: 'User was successfully destroyed.' }
+      format.turbo_stream
+    end
+  end
 end
